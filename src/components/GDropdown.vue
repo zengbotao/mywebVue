59ec9a886898cb246bfe0ed30950acecde1b01c7
zengbotao <2898487084@qq.com>
2022-11-28 20:34:24 +0800

    主页框架

---
+++ src/components/GDropdown.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-dropdown @command="$emit('command',$event)">
+        <span class="el-dropdown-link">
+            {{title}}<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <!-- <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item :key="i" v-for="(item,i) in items" :command="item.key">{{item.label}}</el-dropdown-item>
+        </el-dropdown-menu> -->
+    </el-dropdown>
+
+</template>
+<script>
+export default {
+    name:'GDropdown',
+    // methods:{
+    //     xx(e){
+    //         this.$emit('command',e);
+    //     }
+    // },
+    props: {
+        title: {
+            required: true,
+            type: String
+        },
+        items: {
+            type: Array,
+            required: true,
+        }
+    }
+}
+</script>
+<style>
+</style>
